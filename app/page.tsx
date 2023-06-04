205edfeb4de489e5717772c61c5ad2e922d04a7b
deiji <andrewzakhari5@gmail.com>
2023-06-04 14:55:08 +0300

    Done with the car Preview part

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import Navbar from './components/navbar'
 import Header from './components/header'
+import Cars from './components/cars'
 
 export default function Home() {
   return (
     <div className={styles.main}>
       <Navbar /> 
       <Header />
+      <Cars />
     </div>
   )
 }
